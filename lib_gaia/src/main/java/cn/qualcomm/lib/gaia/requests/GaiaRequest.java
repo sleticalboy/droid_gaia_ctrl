c01771a4fd34443114aab4200a37e137510d3d80
binlee <sleticalboy@gmail.com>
2020-09-19 10:21:45 +0800

    [refactor] rename package to cn.qualcomm

---
+++ lib_gaia/src/main/java/cn/qualcomm/lib/gaia/requests/GaiaRequest.java
@@ -0,0 +1,46 @@
+/**************************************************************************************************
+ * Copyright 2017 Qualcomm Technologies International, Ltd.                                       *
+ **************************************************************************************************/
+
+package cn.qualcomm.lib.gaia.requests;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import androidx.annotation.IntDef;
+
+import cn.qualcomm.lib.gaia.packets.GaiaPacket;
+
+/**
+ * The data structure to define a GAIA request.
+ */
+public class GaiaRequest {
+
+    /**
+     * All types of GAIA requests which can be sent to a device.
+     */
+    @IntDef(flag = true, value = { Type.SINGLE_REQUEST, Type.ACKNOWLEDGEMENT })
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Type {
+        int SINGLE_REQUEST = 1;
+        int ACKNOWLEDGEMENT = 2;
+    }
+
+    /**
+     * The type of the request.
+     */
+    @Type public final int type;
+
+    /**
+     * If this request is about a characteristic, the Bluetooth characteristic for this request.
+     */
+    public GaiaPacket packet;
+
+    /**
+     * To build a new object of the type request.
+     */
+    public GaiaRequest(@Type int type) {
+        this.type = type;
+    }
+
+}
